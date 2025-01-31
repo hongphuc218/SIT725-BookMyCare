10a30a2f14c1ca9d12bb768eff054f6142cab71c
chenmuy7 <chenmuy7@gmail.com>
2025-01-31 15:29:52 +1100

    committed

--- server/server.js
+++ server/server.js
@@ -23,4 +23,4 @@ app.use('/api', require('./routes/appointmentRoutes'));
 
 // Start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
